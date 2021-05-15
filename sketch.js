5b1b7f29650f5b21787b85f0eb6776863d5fe2e5
VT597 <65662921+VT597@users.noreply.github.com>
2021-05-15 22:34:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var ground, groundImg;
+
+var jerry, jerry_run;
+
+var tom, tom_running;
+
+function preload() {
+    //load the images here
+
+    groundImg = loadImage("images/garden.png");
+
+    jerryImg = loadAnimation("images/mouse1.png");
+
+    jerryImage4 = loadAnimation("images/mouse4.png");
+
+    jerry_run = loadAnimation("images/mouse2.png", "images/mouse3.png");
+
+    tomImg1 = loadAnimation("images/cat1.png");
+
+    tomImg4 = loadAnimation("images/cat4.png");
+
+    tom_running = loadAnimation("images/cat2.png", "images/cat3.png");
+
+}
+
+function setup(){
+    createCanvas(680,400);
+    
+    ground = createSprite(340, 200);
+    ground.addImage("groundImg", groundImg);
+    ground.scale = 0.7;
+
+    tom = createSprite(500, 340, 20, 100);
+    tom.addAnimation("tomImg1", tomImg1);
+    tom.scale = 0.1;
+
+    jerry = createSprite(200, 340, 10, 40);
+    jerry.addAnimation("jerryImg", jerryImg);
+    jerry.scale = 0.1;
+
+}
+
+function draw() {
+
+    console.log(tom.x - jerry.x);
+
+    background(rgb(30, 100, 220));
+
+if (tom.x - jerry.x < tom.width / 2 - jerry.width / 2) {
+       tom.velocityX = 0;
+       tom.addAnimation("tomImg4", tomImg4);
+       tom.changeAnimation("tomImg4");
+
+       jerry.addAnimation("jerryImage4", jerryImage4);
+       jerry.changeAnimation("jerryImage4");
+       tom.bounce(jerry);
+       tom.x = 250;
+    }
+
+    drawSprites();
+}
+
+function keyPressed() {
+    
+    if (keyCode === LEFT_ARROW) {
+
+        tom.velocityX = -5;
+        tom.addAnimation("tom_running", tom_running);
+        tom.changeAnimation("tom_running");
+
+        jerry.addAnimation("jerry_run", jerry_run);
+        jerry.changeAnimation("jerry_run");
+    }
+    
+}
